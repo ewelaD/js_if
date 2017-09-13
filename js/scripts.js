7459e9cbf744ba631212b91d80036f2db03bc426
Ewelina Drozda <e.drozda@gmail.com>
2017-09-13 18:44:54 +0200

    dodatkowy warunek #1

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 var a = prompt('Enter a'),
 	b = prompt('Enter b'),
 	value = (a * a) + (2 * a * b) - (b * b);
-alert('Result of calculating is: ' + value);
-if (value < 0) {
-	alert('Wynik ujemny')
-} else if (value == 0) {
-	alert('Wyszło zero')
+if (!(typeof(a) === 'number') && (typeof(b) === 'number')) {	
+	alert('Nieprawidłowe dane');
 } else {
-	alert('Wynik dodatni')
+	alert('Result of calculating is: ' + value);
+}
+alert('Result of calculating is: ' + value);
+if (typeof(value) === 'NaN') {
+	alert('Nieprawidłowe dane');
+} else if (value < 0) {
+	alert('Wynik ujemny');
+} else if (value == 0) {
+	alert('Wyszło zero');
+} else {
+	alert('Wynik dodatni');
 };
 
