ab9b52578ab705f81de3b20632155b7ebfba7a56
Ewelina Drozda <e.drozda@gmail.com>
2017-09-14 18:21:40 +0200

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 var a = prompt('Enter a'),
 	b = prompt('Enter b'),
 	value = (a * a) + (2 * a * b) - (b * b);
-if (!(typeof(a) === 'number') && (typeof(b) === 'number')) {	
+/*if (!(typeof(a) === 'number') && (typeof(b) === 'number')) {	
 	alert('Nieprawidłowe dane');
 } else {
 	alert('Result of calculating is: ' + value);
-}
+}*/
 alert('Result of calculating is: ' + value);
-if (typeof(value) === 'NaN') {
-	alert('Nieprawidłowe dane');
+if (value > 0) {
+	alert('Wynik dodatni');
 } else if (value < 0) {
 	alert('Wynik ujemny');
 } else if (value == 0) {
 	alert('Wyszło zero');
 } else {
-	alert('Wynik dodatni');
+	alert('Nieprawidłowe dane');
 };
 
